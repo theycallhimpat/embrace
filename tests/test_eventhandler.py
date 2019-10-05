3679d1ec4223aa353732b2f4313275bafc9322d1
theycallhimpat <2099137+theycallhimpat@users.noreply.github.com>
2019-10-05 21:52:30 +0100

    Run black on files

--- tests/test_eventhandler.py
+++ tests/test_eventhandler.py
@@ -3,10 +3,11 @@ import unittest
 from .context import embrace
 from embrace import eventhandler
 
+
 class DummyTestSuite(unittest.TestCase):
     def test_dummy(self):
         assert eventhandler.dummy(2, 3) == 5
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
