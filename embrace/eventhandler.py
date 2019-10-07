82de1b76bf9087819640ea15ea837f9c4b18e9c6
theycallhimpat <2099137+theycallhimpat@users.noreply.github.com>
2019-10-07 16:06:33 +0100

    Start adding Event and EventHandler, including tests

--- embrace/eventhandler.py
+++ embrace/eventhandler.py
@@ -1,2 +1,15 @@
 def dummy(x: int, y: int) -> int:
     return x + y
+
+class Event:
+    pass
+
+class EventHandler:
+    def __init__(self):
+        pass
+
+    def has_event(self) -> bool:
+        return False
+
+    def add_event(self, event: Event) -> None:
+        pass
--- tests/test_eventhandler.py
+++ tests/test_eventhandler.py
@@ -8,6 +8,20 @@ class DummyTestSuite(unittest.TestCase):
     def test_dummy(self) -> None:
         assert eventhandler.dummy(2, 3) == 5
 
+class TestEvent(unittest.TestCase):
+    def test_empty(self) -> None:
+        event = eventhandler.Event()
+
+class TestEventHandler(unittest.TestCase):
+    def test_empty(self) -> None:
+        handler = eventhandler.EventHandler()
+        self.assertFalse(handler.has_event())
+
+    def test_add_event(self) -> None:
+        handler = eventhandler.EventHandler()
+        event = eventhandler.Event()
+        handler.add_event(event)
+        self.assertTrue(handler.has_event())
 
 if __name__ == "__main__":
     unittest.main()
