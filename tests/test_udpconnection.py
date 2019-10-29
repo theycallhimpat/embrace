6e77148bf113bde532c373e0eb6242cfa4ab7927
theycallhimpat <2099137+theycallhimpat@users.noreply.github.com>
2019-10-29 23:04:50 +0000

    blacken files

--- tests/test_udpconnection.py
+++ tests/test_udpconnection.py
@@ -1,6 +1,7 @@
 """ unit tests for embrace/udpconnection.py """
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 
 import unittest
 
@@ -8,10 +9,12 @@ from embrace import udpconnection
 
 # pylint: disable=missing-docstring,invalid-name,
 
+
 class TestUDPConnection(unittest.TestCase):
     def test_empty(self) -> None:
         u1 = udpconnection.UDPConnection(("127.0.0.1", 30071), ("127.0.0.1", 56664))
         u2 = udpconnection.UDPConnection(("127.0.0.1", 56664), ("127.0.0.1", 30071))
+
 
 if __name__ == "__main__":
     unittest.main()
